3f52b59ed468fe6bd6057d0b99a0462d301e2a95
RicardoTurco <ricturco@gmail.com>
2024-11-18 11:11:46 -0300

    adding fake users

--- app/constants/constants.py
+++ app/constants/constants.py
@@ -2,3 +2,8 @@ URL_TIVIT_HEALTH = "https://api-onecloud.multicloud.tivit.com/fake/health"
 URL_TIVIT_ADMIN = "https://api-onecloud.multicloud.tivit.com/fake/admin"
 URL_TIVIT_USER = "https://api-onecloud.multicloud.tivit.com/fake/user"
 URL_TIVIT_TOKEN = "https://api-onecloud.multicloud.tivit.com/fake/token"
+
+FAKE_USERS_DB = {
+    "user": {"username": "user", "role": "user", "password": "L0XuwPOdS5U"},
+    "admin": {"username": "admin", "role": "admin", "password": "JKSipm0YH"},
+}
