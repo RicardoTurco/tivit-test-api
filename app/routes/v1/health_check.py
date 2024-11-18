9c123db0daad2a2b212a091a2f1196807ef24dcd
RicardoTurco <ricturco@gmail.com>
2024-11-18 10:58:46 -0300

    creating endpoint to check external api

--- app/routes/v1/health_check.py
+++ app/routes/v1/health_check.py
@@ -1,9 +1,22 @@
 from fastapi import APIRouter, Response, status
 
+from app.services.tivit_fake_service import TivitFakeService
+
 
 health_router = APIRouter()
 
 tag = "Health Check"
+
+
+@health_router.get("/external-health-check", tags=[tag], summary="Check status of external application")
+async def external_health_check(response: Response):
+    """
+    This endpoint returns a health check of external application.
+    """
+    health_check_external = await TivitFakeService.health_check()
+    result = True if health_check_external else False
+    response.status_code = status.HTTP_200_OK
+    return {"external_application": result}
 
 
 @health_router.get("/health-check", tags=[tag], summary="Check status of application")
