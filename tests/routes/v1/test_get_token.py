db08cf8ab97d5a91c17012d6fdc72cf6165f885b
RicardoTurco <ricturco@gmail.com>
2024-11-19 00:07:26 -0300

    refactor in get token tests

--- tests/routes/v1/test_get_token.py
+++ tests/routes/v1/test_get_token.py
@@ -1,4 +1,5 @@
 import pytest
+from fastapi import status
 from unittest.mock import AsyncMock, patch
 
 from app.schemas.token import TokenCredentials
@@ -16,11 +17,13 @@ async def test_get_token_success(client):
         "token_type": "bearer"
     }
 
-    with patch("app.services.tivit_fake_service.TivitFakeService.get_token", new_callable=AsyncMock) as mock_get_token:
+    with patch("app.services.tivit_fake_service.TivitFakeService.get_token",
+               new_callable=AsyncMock) as mock_get_token:
+
         mock_get_token.return_value = mock_token_data
         response = client.post("/v1/get-token", json=mock_token_credentials)
 
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
         assert response.json() == {
             "access_token": "mock_access_token",
             "token_type": "bearer"
