be0b1e334d991924b51e4e6f328b667a363c847b
RicardoTurco <ricturco@gmail.com>
2025-02-16 23:26:49 -0300

    create health check tests

---
+++ tests/routes/v1/test_health_check.py
@@ -0,0 +1,30 @@
+from unittest.mock import AsyncMock, patch
+
+import pytest
+from fastapi import status
+
+
+@pytest.mark.asyncio
+async def test_external_health_check_success(client):
+
+    mock_external_app_result = {"external_application": True}
+
+    with patch(
+        "app.services.tivit_fake_service.TivitFakeService.health_check",
+        new_callable=AsyncMock,
+    ) as mock_external_health_check:
+
+        mock_external_health_check.return_value = mock_external_app_result
+        response = client.get("/v1/external-health-check")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {"external_application": True}
+
+
+@pytest.mark.asyncio
+async def test_health_check_success(client):
+
+    response = client.get("/v1/health-check")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"application": True}
